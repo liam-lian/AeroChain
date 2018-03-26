ec7200cb5007260ba873b751a22c3ef996748b6b
丁斯也 <kaka940307@vip.qq.com>
2018-03-26 11:15:34 +0800

    进行分布式测试

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -14,12 +14,15 @@ public class Constant {
 
     public static final int CHECKPOINT = 60;
 
-    public static final int BLOCK_GAP = 6;
+    public static final int BLOCK_GAP = 3;
 
     public static final String SEPARTOR = "- - - - - - - - - -";
 
     public static List<String> address = new ArrayList<>();
     static {
         address.add("127.0.0.1");
+        address.add("192.168.0.124");
+        address.add("192.168.0.127");
+        address.add("192.168.0.116");
     }
 }
