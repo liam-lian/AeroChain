b756a97b39b046da1fdf7631786b56ff74506912
丁斯也 <kaka940307@vip.qq.com>
2018-03-29 09:13:45 +0800

    主流程通过

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -22,6 +22,7 @@ public class Constant {
 
     public static List<String> address = new ArrayList<>();
     static {
+        address.add("127.0.0.1");
         address.add("192.168.0.112");
         address.add("192.168.0.124");
         address.add("192.168.0.127");
